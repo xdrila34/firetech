24105d65dfb4b9609113fbc2716b44a22a254f6a
yllberishaa <yllberisha67@gmail.com>
2023-10-25 10:34:28 +0200

    added product page and style

---
+++ front page/css/main.js
@@ -0,0 +1,7 @@
+function redirectToBuyNow() {
+    window.location.href = ""; // Replace with your desired URL
+}
+
+function addToCart() {
+    window.location.href = "https://www.example.com/add-to-cart"; // Replace with your desired URL
+}
